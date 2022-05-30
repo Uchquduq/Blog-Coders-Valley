24f606eca3fb41f8910ba8203f4df83f2bf25ddd
Rufat <rufatxasanov1998@gmail.com>
2022-05-30 15:27:20 +0500

    TagFeed Toggler qo'shildi

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -4,6 +4,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
+          <mcv-feed-toggler />
           <mcv-feed :api-url="apiUrl"></mcv-feed>
         </div>
         <div class="col-md-3">
@@ -18,12 +19,14 @@
 import McvPopularTags from "@/components/PopularTags";
 import McvFeed from "@/components/Feed";
 import McvBanner from "@/components/Banner";
+import McvFeedToggler from "@/components/FeedToggler";
 export default {
   name: "McvGlobalFeed",
   components: {
     McvFeed,
     McvPopularTags,
     McvBanner,
+    McvFeedToggler,
   },
   data() {
     return {
--- src/views/TagFeed.vue
+++ src/views/TagFeed.vue
@@ -4,6 +4,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
+          <mcv-feed-toggler :tag-name="tagName" />
           <mcv-feed :api-url="apiUrl"></mcv-feed>
         </div>
         <div class="col-md-3">
@@ -18,18 +19,21 @@
 import McvPopularTags from "@/components/PopularTags";
 import McvFeed from "@/components/Feed";
 import McvBanner from "@/components/Banner";
+import McvFeedToggler from '@/components/FeedToggler'
 export default {
   name: "McvTagFeed",
   components: {
     McvFeed,
     McvPopularTags,
     McvBanner,
+    McvFeedToggler
   },
   computed: {
+    tagName() {
+      return this.$route.params.slug;
+    },
     apiUrl() {
-      const tagName = this.$route.params.slug;
-      console.log("tagName", tagName);
-      return `/articles?tag=${tagName}`;
+      return `/articles?tag=${this.tagName}`;
     },
   },
 };
